3326074fb00d02ddbb4833b712e828008cf41054
anlanbupang <anlanbupang@vip.qq.com>
2024-06-10 00:57:03 +0800

    add qq config

--- config/index.js
+++ config/index.js
@@ -70,6 +70,34 @@ export default defineConfig(async (merge, { command, mode }) => {
         }
       }
     },
+    qq: {
+      webpackChain(chain) {
+        chain.plugin('unplugin-vue-components').use(ComponentsPlugin({
+          resolvers: [NutUIResolver({taro: true})]
+        }))
+      },
+      postcss: {
+        pxtransform: {
+          enable: true,
+          config: {
+
+          }
+        },
+        url: {
+          enable: true,
+          config: {
+            limit: 1024 // 设定转换尺寸上限
+          }
+        },
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      }
+    },
     h5: {
       webpackChain(chain) {
         chain.plugin('unplugin-vue-components').use(ComponentsPlugin({
