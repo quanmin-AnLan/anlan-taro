a0273922085f1a8c7d7d678cf310ffe555ce77e5
wnw <ningweiwang218830@sohu-inc.com>
2024-07-02 17:26:09 +0800

    qq小程序去掉联系我们

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="home-container">
       <AlButton text="全民大乐斗" @click="Taro.navigateTo({url: '/penguinFight/pages/index/index'})"></AlButton>
-      <AlButton text="联系我们" @click="Taro.navigateTo({url: '/pages/contact/index'})"></AlButton>
+      <AlButton text="联系我们" @click="Taro.navigateTo({url: '/pages/contact/index'})" v-if="TARO_ENV !== 'qq'"></AlButton>
     </div>
     <div class="home-bottom">
       <text class="bottom-left">Ciallo～(∠・ω • )⌒★</text>
@@ -17,6 +17,7 @@
 import Taro from '@tarojs/taro'
 import AlButton from '@/components/AlButton.vue'
 import './index.less'
+const { TARO_ENV } = process.env
 
 const shareData = {
   title: 'Anlan工具箱',
